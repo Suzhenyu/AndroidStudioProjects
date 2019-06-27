1559ad3730e42eacd572d6092779db8255f2ea35
suzhenyu <983424749@qq.com>
2019-06-27 11:32:18 +0800

    Button 的用法

--- UIWidgetTest/app/src/main/java/io/github/suzhenyu/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/io/github/suzhenyu/uiwidgettest/MainActivity.java
@@ -3,6 +3,10 @@ package io.github.suzhenyu.uiwidgettest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 使用匿名类来注册监听器
+        Button button1 = (Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 在此添加点击逻辑
+            }
+        });
+
+//        // 这种方法不行，不知为何
+//        Button button2 = (Button)findViewById(R.id.button);
+//        button2.setOnClickListener(this);
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.button:
+//                // 在此添加点击逻辑
+//                Log.d("MainActivity", "onClick");
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 }

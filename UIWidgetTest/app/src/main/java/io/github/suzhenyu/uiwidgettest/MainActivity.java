3705a9f3c8e426bb7312b6a46dbd37cb513e9aab
suzhenyu <983424749@qq.com>
2019-06-27 11:59:49 +0800

    EditText 的用法

--- UIWidgetTest/app/src/main/java/io/github/suzhenyu/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/io/github/suzhenyu/uiwidgettest/MainActivity.java
@@ -6,38 +6,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText)findViewById(R.id.edit_text);
         // 使用匿名类来注册监听器
         Button button1 = (Button)findViewById(R.id.button);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 在此添加点击逻辑
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
             }
         });
-
-//        // 这种方法不行，不知为何
-//        Button button2 = (Button)findViewById(R.id.button);
-//        button2.setOnClickListener(this);
     }
-
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()) {
-//            case R.id.button:
-//                // 在此添加点击逻辑
-//                Log.d("MainActivity", "onClick");
-//                break;
-//            default:
-//                break;
-//        }
-//    }
 }

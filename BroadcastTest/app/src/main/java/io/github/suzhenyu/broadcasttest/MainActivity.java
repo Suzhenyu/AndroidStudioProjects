11435ac3e377c8643f90f8605abc990118700575
suzhenyu <983424749@qq.com>
2019-07-01 12:12:49 +0800

    “接收系统广播”示例代码

---
+++ BroadcastTest/app/src/main/java/io/github/suzhenyu/broadcasttest/MainActivity.java
@@ -0,0 +1,56 @@
+package io.github.suzhenyu.broadcasttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+
+    private NetworkChangeReceiver networkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+//            Toast.makeText(context, "Network changes", Toast.LENGTH_SHORT).show();
+
+            ConnectivityManager connectionManager
+                    = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "Network is available", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Network is unavailable", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+}
